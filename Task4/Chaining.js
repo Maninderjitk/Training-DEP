c52e986b8f589e5851984b738aead709c5b0191e
Maninderjit Kaur <maninderjit_kaur@epam.com>
2020-03-16 17:16:26 +0530

    changes in task 4

--- Task4/Chaining.js
+++ Task4/Chaining.js
@@ -1,10 +1,10 @@
 // 2-https://www.codewars.com/kata/539a0e4d85e3425cb0000a88/train/javascript
-function add(n) {
-  let fn = function (x) {
-    return add(n + x);
+function add(argu) {
+  let fn = function (agruNext) {
+    return add(argu + agruNext);
   };
   fn.valueOf = function () {
-    return n;
+    return argu;
   };
   return fn;
 }
